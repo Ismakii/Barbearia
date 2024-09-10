ecf5bbeb37e7f7763085309de5e519e854943fa8
Ismakii <Ismaelglaydson46@gmail.com>
2024-09-10 13:33:41 -0300

    Adicionado um background que ainda tem que ser ajustado

--- scripts.js
+++ scripts.js
@@ -1,34 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const defaultLink = document.getElementById("inicio");
-  defaultLink.classList.add("active");
+  const links = document.querySelectorAll(".opcoes ul li a");
   const iframe = document.getElementById("iframe-content");
-  iframe.src = "opcoes/inicio.html";
-});
 
-document.querySelectorAll(".opcoes ul li a").forEach((item) => {
-  item.addEventListener("click", function (event) {
-    event.preventDefault();
-    document.querySelectorAll(".opcoes ul li a").forEach((link) => {
-      link.classList.remove("active");
+  const updateContent = (id) => {
+    links.forEach(link => link.classList.toggle("active", link.id === id));
+    iframe.src = `opcoes/${id}.html`;
+  };
+
+  updateContent("inicio");
+
+  links.forEach(link => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      updateContent(link.id);
     });
-    this.classList.add("active");
-    const iframe = document.getElementById("iframe-content");
-    switch (this.id) {
-      case "inicio":
-        iframe.src = "opcoes/inicio.html";
-        break;
-      case "servicos":
-        iframe.src = "opcoes/servicos.html";
-        break;
-      case "agendamentos":
-        iframe.src = "opcoes/agendamentos.html";
-        break;
-      case "produtos":
-        iframe.src = "opcoes/produtos.html";
-        break;
-      case "contatos":
-        iframe.src = "opcoes/contatos.html";
-        break;
-    }
   });
 });
